665ad16e313f540ef83de2da8e7ae4f296bf627e
S0lace11 <136303284@qq.com>
2025-08-29 18:29:21 +0800

    fix error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+type Props = React.ComponentProps<typeof NextThemesProvider>
+
+export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
